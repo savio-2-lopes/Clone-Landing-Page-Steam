243429e32913d6611723883170ebd26819e972f9
savio-2-lopes <savioaugulopes@gmail.com>
2022-09-08 11:12:22 -0300

    Apply Loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,12 +11,14 @@ import { Link } from "react-router-dom";
 import HeadTitle from "../components/HeadTitle";
 import Logo from "../components/Logo";
 import CardTitle from "../components/CardTitle";
+import { Spinner } from "../components/Spinner";
 
 export function Home() {
   const [showButton, setShowButton] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const [showData, setShowData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const data = `https://api.rawg.io/api/games?key=${
     import.meta.env.VITE_API_KEY
   }`;
@@ -24,6 +26,7 @@ export function Home() {
   useEffect(() => {
     axios.get(data).then((response) => {
       setShowData(response.data.results);
+      setLoading(true);
     });
   });
 
@@ -232,210 +235,215 @@ export function Home() {
         )}
       </nav>
 
-      {/* Header */}
-      <Swiper
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {showData.map((element, index) => (
-          <SwiperSlide key={index}>
-            <div
-              className="min-h-[70vh] flex"
-              style={{
-                background: `linear-gradient(266.12deg, rgba(14, 19, 30, 0) 0%, rgba(14, 19, 30, 0.7) 47.21%, rgba(14, 19, 30, 0.7) 86.62%, rgba(14, 19, 30, 0) 100%), url(${element["background_image"]})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-              }}
-            >
-              <div className="container mx-auto flex items-center">
-                <div className="max-w-[360px]">
-                  <HeadTitle />
-                  <p className="mt-[32.76px] text-[18px] font-[500] text-white leading-relaxed">
-                    Estados Unidos, 1899. O fim da era do velho oeste começou, e
-                    as autoridades estão caçando as últimas gangues de fora da
-                    lei que restam. Os que não se rendem, nem sucumbem, são
-                    mortos.
-                  </p>
+      {loading ? (
+        <>
+          {/* Header */}
+          <Swiper
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+            className="mySwiper"
+          >
+            {showData.map((element, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className="min-h-[70vh] flex"
+                  style={{
+                    background: `linear-gradient(266.12deg, rgba(14, 19, 30, 0) 0%, rgba(14, 19, 30, 0.7) 47.21%, rgba(14, 19, 30, 0.7) 86.62%, rgba(14, 19, 30, 0) 100%), url(${element["background_image"]})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                  }}
+                >
+                  <div className="container mx-auto flex items-center">
+                    <div className="max-w-[360px]">
+                      <HeadTitle />
+                      <p className="mt-[32.76px] text-[18px] font-[500] text-white leading-relaxed">
+                        Estados Unidos, 1899. O fim da era do velho oeste
+                        começou, e as autoridades estão caçando as últimas
+                        gangues de fora da lei que restam. Os que não se rendem,
+                        nem sucumbem, são mortos.
+                      </p>
 
-                  <Link to={element["slug"]}>
-                    <button className="mt-[32px] w-[229px] h-[55px] bg-blue-400 hover:opacity-90 uppercase py-4 rounded font-bold text-sm transition-colors disabled:opacity-50">
-                      Compre Agora
-                    </button>
-                  </Link>
+                      <Link to={element["slug"]}>
+                        <button className="mt-[32px] w-[229px] h-[55px] bg-blue-400 hover:opacity-90 uppercase py-4 rounded font-bold text-sm transition-colors disabled:opacity-50">
+                          Compre Agora
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
+          {/* Menu */}
+          <section className="container mx-auto bg-blue-700 mt-10">
+            <div className="pb-[10px] pt-[10px] flex">
+              <div className="relative flex h-16 items-center">
+                <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
+                  <button
+                    type="button"
+                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    aria-controls="mobile-menu"
+                    onClick={() =>
+                      showMenu ? setShowMenu(false) : setShowMenu(true)
+                    }
+                    aria-expanded="false"
+                  >
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                      />
+                    </svg>
+
+                    <svg
+                      className="hidden h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+
+                <div className="flex items-center justify-center lg:items-stretch lg:justify-start">
+                  <div className="hidden lg:ml-6 lg:block">
+                    <div className="flex space-x-4">
+                      <a
+                        href="#"
+                        className="text-blue-500 uppercase text-[18px] border-4 mb-[-1.40rem] border border-blue-700 border-b-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                        aria-current="page"
+                      >
+                        Novidades e tendências
+                      </a>
+                      <a
+                        href="#"
+                        className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                      >
+                        categorias
+                      </a>
+                      <a
+                        href="#"
+                        className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                      >
+                        loja de pontos
+                      </a>
+                      <a
+                        href="#"
+                        className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                      >
+                        notícias
+                      </a>
+                      <a
+                        href="#"
+                        className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                      >
+                        laboratório
+                      </a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
 
-      {/* Menu */}
-      <section className="container mx-auto bg-blue-700 mt-10">
-        <div className="pb-[10px] pt-[10px] flex">
-          <div className="relative flex h-16 items-center">
-            <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
-              <button
-                type="button"
-                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-blue-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                onClick={() =>
-                  showMenu ? setShowMenu(false) : setShowMenu(true)
-                }
-                aria-expanded="false"
-              >
-                <svg
-                  className="block h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
-
-                <svg
-                  className="hidden h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            <div className="flex items-center justify-center lg:items-stretch lg:justify-start">
-              <div className="hidden lg:ml-6 lg:block">
-                <div className="flex space-x-4">
+            {showMenu && (
+              <div className="lg:hidden" id="mobile-menu">
+                <div className="space-y-1 px-2 pt-2 pb-3">
                   <a
                     href="#"
-                    className="text-blue-500 uppercase text-[18px] border-4 mb-[-1.40rem] border border-blue-700 border-b-blue-500 px-3 py-2 rounded-md text-sm font-medium"
-                    aria-current="page"
+                    className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
                   >
                     Novidades e tendências
                   </a>
+
                   <a
                     href="#"
-                    className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
                   >
                     categorias
                   </a>
+
                   <a
                     href="#"
-                    className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
                   >
                     loja de pontos
                   </a>
+
                   <a
                     href="#"
-                    className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
                   >
                     notícias
                   </a>
+
                   <a
                     href="#"
-                    className="text-white uppercase text-[18px] border-4 hover:mb-[-1.40rem] border border-blue-700 hover:border-b-blue-500 hover:text-blue-500 px-3 py-2 rounded-md text-sm font-medium"
+                    className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
                   >
                     laboratório
                   </a>
                 </div>
               </div>
-            </div>
+            )}
+          </section>
+
+          {/* Search */}
+          <div className="mt-10">
+            <Search />
           </div>
-        </div>
 
-        {showMenu && (
-          <div className="lg:hidden" id="mobile-menu">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-              <a
-                href="#"
-                className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
-              >
-                Novidades e tendências
-              </a>
-
-              <a
-                href="#"
-                className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
-              >
-                categorias
-              </a>
-
-              <a
-                href="#"
-                className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
-              >
-                loja de pontos
-              </a>
-
-              <a
-                href="#"
-                className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
-              >
-                notícias
-              </a>
-
-              <a
-                href="#"
-                className="text-white uppercase hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
-              >
-                laboratório
-              </a>
-            </div>
+          {/* Gallery */}
+          <div className="container mx-auto mt-10 mb-10">
+            <Swiper
+              slidesPerView={4}
+              spaceBetween={30}
+              modules={[Pagination]}
+              className="mySwiper"
+            >
+              {showData.map((element, index) => (
+                <SwiperSlide key={index}>
+                  <Link to={element["slug"]}>
+                    <div
+                      className="flex items-end justify-center"
+                      style={{
+                        background: `linear-gradient(180deg, rgba(12, 12, 12, 0) 51.56%, #0C0C0C 100%), url(${element["background_image"]})`,
+                        backgroundRepeat: "no-repeat",
+                        width: "280px",
+                        height: "450px",
+                      }}
+                    >
+                      <CardTitle />
+                    </div>
+                  </Link>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
-        )}
-      </section>
-
-      {/* Search */}
-      <div className="mt-10">
-        <Search />
-      </div>
-
-      {/* Gallery */}
-      <div className="container mx-auto mt-10 mb-10">
-        <Swiper
-          slidesPerView={4}
-          spaceBetween={30}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {showData.map((element, index) => (
-            <SwiperSlide key={index}>
-              <Link to={element["slug"]}>
-                <div
-                  className="flex items-end justify-center"
-                  style={{
-                    background: `linear-gradient(180deg, rgba(12, 12, 12, 0) 51.56%, #0C0C0C 100%), url(${element["background_image"]})`,
-                    backgroundRepeat: "no-repeat",
-                    width: "280px",
-                    height: "450px",
-                  }}
-                >
-                  <CardTitle />
-                </div>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-
+        </>
+      ) : (
+        <Spinner />
+      )}
       <Footer />
     </>
   );
